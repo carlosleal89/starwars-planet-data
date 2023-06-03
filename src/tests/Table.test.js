09909b5084e32e67851f97d008df8e10d043c2bb
Carlos Leal <carlos.frleal@gmail.com>
2023-06-02 23:15:35 -0300

    requisito 5

--- src/tests/Table.test.js
+++ src/tests/Table.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen, waitFor, act } from '@testing-library/react';
+import { render, screen, waitFor, act, waitForElementToBeRemoved } from '@testing-library/react';
 import App from '../App';
 import PlanetsProvider from '../context/PlanetsProvider';
 import mockData from '../mocks/data';
@@ -7,7 +7,7 @@ import mockData from '../mocks/data';
 describe('Testes do componente Table', () => {
     beforeEach(() => {
         jest.spyOn(global, "fetch");
-        global.fetch = jest.fn().mockResolvedValue({
+        global.fetch.mockResolvedValue({
             json: jest.fn().mockResolvedValue(mockData),
         });
 
@@ -18,17 +18,11 @@ describe('Testes do componente Table', () => {
         );
     })
 
-    afterEach(() => {
-        global.fetch.mockRestore();
-    });
+    afterEach(jest.restoreAllMocks);
 
-    test('Testa se os elementos do cabeçalho da tabela estão presentes.', () => {
-      act(async () => {
-          const headersEl = await screen.findAllByRole('columnheader');
-          const diameterHeader = screen.getByRole('columnheader', { name: /diameter/i});
-          expect(diameterHeader).toBeInTheDocument();
-          expect(headersEl.toBeInTheDocument());
-      })  
-    //   expect(nameHeader).toBeInTheDocument();
+    test('Testa se os elementos do cabeçalho da tabela estão presentes.', async () => {
+    await waitForElementToBeRemoved(() => screen.queryByText(/Carregando/i));
+    const planet = screen.getByRole('cell', {  name: /tatooine/i});
+    expect(planet).toBeInTheDocument();
     });
-    })
+})
